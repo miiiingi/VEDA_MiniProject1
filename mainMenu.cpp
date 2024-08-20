45c2eac2c643348f5b0fc021391153aa94f92bd3
miiiingi <kimmingi722@gmail.com>
2024-08-20 15:16:34 +0900

    feat: item 관련 콘솔 추가

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -1,20 +1,25 @@
 #include "mainMenu.h"
-#include "customer.h"  
+#include "customer.h"
+#include "itemManager.h"
+#include "item.h"
 
 #include <iostream>
 
 using namespace std;
 
-void MainMenu::run() {
+void MainMenu::run()
+{
     int choice = 0;
-    do {
+    do
+    {
         showMainMenu();
         cin >> choice;
         handleMainMenuChoice(choice);
-    } while (choice != 4);  
+    } while (choice != 4);
 }
 
-void MainMenu::showMainMenu() {
+void MainMenu::showMainMenu()
+{
     cout << "\n=== Welcome to VEDA mall ===" << endl;
     cout << "1. Customer" << endl;
     cout << "2. Item" << endl;
@@ -23,8 +28,10 @@ void MainMenu::showMainMenu() {
     cout << "Enter your choice >> ";
 }
 
-void MainMenu::handleMainMenuChoice(int choice) {
-    switch (choice) {
+void MainMenu::handleMainMenuChoice(int choice)
+{
+    switch (choice)
+    {
     case 1:
         showCustomerMenu();
         break;
@@ -43,9 +50,11 @@ void MainMenu::handleMainMenuChoice(int choice) {
     }
 }
 
-void MainMenu::showCustomerMenu() {
+void MainMenu::showCustomerMenu()
+{
     int choice = 0;
-    do {
+    do
+    {
         cout << "\n=== Customer Menu ===" << endl;
         cout << "1. Insert Customer" << endl;
         cout << "2. Search Customer" << endl;
@@ -58,9 +67,12 @@ void MainMenu::showCustomerMenu() {
     } while (choice != 5);
 }
 
-void MainMenu::handleCustomerMenuChoice(int choice) {
-    switch (choice) {
-    case 1: {       // 1. Insert Costomer
+void MainMenu::handleCustomerMenuChoice(int choice)
+{
+    switch (choice)
+    {
+    case 1:
+    { // 1. Insert Costomer
         unsigned int id;
         string name, phone;
 
@@ -71,60 +83,93 @@ void MainMenu::handleCustomerMenuChoice(int choice) {
         cout << "Successfully added customer information" << endl;
         break;
     }
-    case 2:  {      // 2. Search Customer
+    case 2:
+    { // 2. Search Customer
         string phone;
         cout << "Enter customer phone number to search >> " << endl;
         cin >> phone;
 
-        unique_ptr<Customer>& customer = customerManager.searchCustomer(phone);
+        unique_ptr<Customer> &customer = customerManager.searchCustomer(phone);
 
-        if (customer) {
+        if (customer)
+        {
             cout << "Customer Found" << endl;
             cout << "ID: " << customer->getCustomerId() << endl;
             cout << "Name: " << customer->getCustomerName() << endl;
             cout << "Phone: " << customer->getCustomerPhoneNumber() << endl;
         }
-        else {
+        else
+        {
             cout << "Customer not found ..." << endl;
         }
         break;
     }
     case 3:
-        cout << "Modify Customer (T.B.D)..." << endl; 
+        cout << "Modify Customer (T.B.D)..." << endl;
         break;
     case 4:
-        cout << "Delete Customer (T.B.D)..." << endl; 
+        cout << "Delete Customer (T.B.D)..." << endl;
         break;
     case 5:
-        break; 
+        break;
     default:
         cout << "Invalid choice. Please try again." << endl;
         break;
     }
 }
 
-void MainMenu::showItemMenu() {
+void MainMenu::showItemMenu()
+{
     int choice = 0;
-    do {
+    do
+    {
         cout << "\n=== Item Menu ===" << endl;
-        cout << "1. View Item list" << endl;
-        cout << "2. T.B.D" << endl;
-        cout << "3. Back to Main Menu" << endl;
+        cout << "1. Add Item" << endl;
+        cout << "2. Get Item" << endl;
+        cout << "3. Update Item" << endl;
+        cout << "4. Delete Item" << endl;
+        cout << "5. Back to Main Menu" << endl;
         cout << "Enter your choice >> ";
         cin >> choice;
         handleItemMenuChoice(choice);
-    } while (choice != 3);  
+    } while (choice != 5);
 }
 
-void MainMenu::handleItemMenuChoice(int choice) {
-    switch (choice) {
+void MainMenu::handleItemMenuChoice(int choice)
+{
+    string barcode, manufacturer;
+    ItemManager itemManager;
+    Item* item;
+    switch (choice)
+    {
     case 1:
-        cout << "View Item list..." << endl; 
+        cout << "Type barcode and manufacturer >> ";
+        cin >> barcode >> manufacturer;
+        itemManager.addItem(barcode, manufacturer);
         break;
     case 2:
-        cout << "to be determined..." << endl; 
+        cout << "Type barcode >> ";
+        cin >> barcode;
+        item = itemManager.getItem(barcode);
+        cout << "Barcode: " << item->getbarcodeNumber() << endl;
+        cout << "Manufacturer: " << item->getItemManufacturer();
         break;
     case 3:
+        cout << "Type barcode and manufacturer >> ";
+        cin >> barcode >> manufacturer;
+        itemManager.updateItem(barcode, manufacturer);
+        cout << "Updated Barcode: " << barcode;
+        break;
+    case 4:
+        cout << "Type barcode >> ";
+        cin >> barcode;
+        itemManager.deleteItem(barcode);
+        cout << "Deleted Barcode: " << barcode;
+        break;
+    case 5:
+        break;
+    default:
+        cout << "Invalid choice. Please try again." << endl;
         break;
     }
 }
