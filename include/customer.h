c2ef8313628ca6b5c37320bf3b2b1eb706c2f692
Kevin Lee <ajoa4936@gmail.com>
2024-08-23 19:52:38 +0900

    fix:fixed customer class to not handle id directly. / fixed customerManager class to not insert point values so that point member can be set as 0 automatically.

--- include/customer.h
+++ include/customer.h
@@ -8,10 +8,10 @@ class Customer{
 
 
 public:
-    Customer(unsigned int,const std::string&,const std::string&, unsigned int);
+    Customer(const std::string&,const std::string&, unsigned int);
 
-    void setCustomerId(const unsigned int);
-    unsigned int getCustomerId() const;
+    //void setCustomerId(const unsigned int);
+    //unsigned int getCustomerId() const;
 
     void setCustomerName(const std::string&);
     const std::string& getCustomerName() const ;
@@ -23,12 +23,10 @@ public:
     unsigned int getCustomerPoint() const;
 
 private:
-    unsigned int customerId;
+    //unsigned int customerId;
     std::string customerName;
     std::string customerPhoneNumber;
     unsigned int customerPoint;
-    //구매내역은 어떻게 구성하면 좋을지 아이디어 필요
-    //아이템 세일즈정보와 / 세부정보를 분리했다고 하자.
 
 };
 
