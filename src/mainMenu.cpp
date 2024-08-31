43e0b42742faeb3e9d7f3188c5d0668a87379dd3
miiiingi <kimmingi722@gmail.com>
2024-08-31 18:54:36 +0900

    fix: 들여쓰기 수정

--- src/mainMenu.cpp
+++ src/mainMenu.cpp
@@ -152,8 +152,8 @@ void MainMenu::showCustomerManagementMenu()
         cout << "2. Search Customer" << endl;
         cout << "3. Update Customer" << endl;
         cout << "4. Delete Customer" << endl;
-	cout << "5. Import Customers from CSV" << endl;
-	cout << "6. Export Customers to CSV" << endl;
+        cout << "5. Import Customers from CSV" << endl;
+        cout << "6. Export Customers to CSV" << endl;
         cout << "7. Back to Manager Menu" << endl;
         cout << "Enter your choice >> ";
         cin >> choice;
@@ -246,25 +246,28 @@ void MainMenu::handleCustomerManagementMenuChoice(int choice)
     }
     case 5:
     {
-	char load_ans;
-	cout<<"Warning : Your current customer data will be lost. Do you agree? [Y/n]  >>";
-	cin>>load_ans;
-	if(load_ans=='Y'||load_ans=='y'){
-	    cout<<"Importing Customer List from CSV file to database " << endl;
-	    customerManager.loadFromCSV("customerList.csv");
-	    break;
-	}else{
-	    cout<<"CSV Import aborted"<< endl;
-	    break;
-	}
-	break;
+        char load_ans;
+        cout << "Warning : Your current customer data will be lost. Do you agree? [Y/n]  >>";
+        cin >> load_ans;
+        if (load_ans == 'Y' || load_ans == 'y')
+        {
+            cout << "Importing Customer List from CSV file to database " << endl;
+            customerManager.loadFromCSV("customerList.csv");
+            break;
+        }
+        else
+        {
+            cout << "CSV Import aborted" << endl;
+            break;
+        }
+        break;
     }
     case 6:
     {
 
-	cout<<"Exporting Customer List from database to CSV file " << endl;
-	customerManager.saveToCSV("customerList.csv");
-	break;
+        cout << "Exporting Customer List from database to CSV file " << endl;
+        customerManager.saveToCSV("customerList.csv");
+        break;
     }
     case 7:
         cout << "Returning to Manager Menu..." << endl;
